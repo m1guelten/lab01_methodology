e3cae64543adee59d6c9824c6c5df2714f50d36c
m1guelten <footballmiguelten@gmail.com>
2023-03-18 13:46:23 +0200

    create interactive mode

--- index.js
+++ index.js
@@ -30,8 +30,48 @@ const nonInteractive = () => {
   }
 };
 
+const interactive = () => {
+  const requests = ['Input a: ', 'Input b: ', 'Input c: '];
+  const NUM_REQUEST = requests.length;
+  const params = [];
+
+  const question = (i) => {
+    process.stdout.write(requests[i]);
+  };
+
+  question(0);
+  process.stdin.on('data', (data) => {
+    if (params.length < NUM_REQUEST) {
+      if (!isNum(data)) {
+        console.log(
+          `Error. Expected a valid real number, got ${data
+            .toString()
+            .trim()} instead`
+        );
+
+        question(params.length);
+      } else if (data.toString().trim() === 0 && params.length === 0) {
+        console.log(`Error. a cannot be 0`);
+        question(params.length);
+      } else {
+        params.push(+data.toString().trim());
+        if (params.length < NUM_REQUEST) {
+          question(params.length);
+        } else {
+          const [a, b, c] = params;
+          console.log(`\n a=${a} b=${b} c=${c} `);
+          process.exit();
+        }
+      }
+    }
+  });
+};
+
 const formatChecking = (data) => {
   return /^-?\d+.?\d*\s-?\d+.?\d*\s-?\d+.?\d*/.test(data);
 };
 
-nonInteractive();
+const isNum = (arg) => !isNaN(parseFloat(arg.toString().trim()));
+
+//nonInteractive();
+interactive();
